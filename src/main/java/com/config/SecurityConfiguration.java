92eaf56717f1af812468c7a9b76278310c5872c1
Robinson Cruz <robcruzd@hotmail.com>
2020-07-17 22:36:04 -0500

    api with authenticated

--- src/main/java/com/config/SecurityConfiguration.java
+++ src/main/java/com/config/SecurityConfiguration.java
@@ -89,7 +89,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
             .antMatchers("/api/registrar").permitAll()
             .antMatchers("/api/tipo-documentos").permitAll()
             .antMatchers("/api/hoja-vida/0").permitAll()
-            .antMatchers("/api/**").permitAll()
+            .antMatchers("/api/**").authenticated()
             .antMatchers("/management/health").permitAll()
             .antMatchers("/management/info").permitAll()
             .antMatchers("/management/prometheus").permitAll()
