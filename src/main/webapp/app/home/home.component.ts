40e9b36bf32225d4e91240e466013557ca8831c8
Robinson Cruz <robcruzd@hotmail.com>
2021-01-09 21:29:11 -0800

    home con buscar e ir a registro empresa

--- src/main/webapp/app/home/home.component.ts
+++ src/main/webapp/app/home/home.component.ts
@@ -1,10 +1,17 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { Subscription } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 import { LoginModalService } from 'app/core/login/login-modal.service';
 import { AccountService } from 'app/core/auth/account.service';
 import { Account } from 'app/core/user/account.model';
 import { Router, NavigationEnd } from '@angular/router';
 import { IRegiones } from 'app/shared/model/regiones.model';
+import { RegionesService } from 'app/entities/regiones/regiones.service';
+import { HttpResponse } from '@angular/common/http';
+import { IProfesion } from 'app/shared/model/profesion.model';
+import { FormControl } from '@angular/forms';
+import { ProfesionService } from 'app/entities/profesion/profesion.service';
+import { map, startWith } from 'rxjs/operators';
+import { commonMessages } from 'app/shared/constants/commonMessages';
 
 declare let gtag: Function;
 
@@ -17,8 +24,24 @@ export class HomeComponent implements OnInit, OnDestroy {
   account: Account | null = null;
   authSubscription?: Subscription;
   region: Array<IRegiones> = [];
+  ofertas = [];
+  data: any = [];
+  filteredOptionsCiudades = new Observable<string[]>();
+  filteredOptionsProfesiones = new Observable<IProfesion[]>();
+  myControlCiudades = new FormControl();
+  myControlProfesiones = new FormControl();
+  ciudades: string[] = [];
+  profesiones: Array<IProfesion> = [];
+  lblSeleccioneProfesion = commonMessages.SELECCIONE_PROFESION_LABEL;
+  lblSeleccioneCiudad = commonMessages.SELECCIONE_CIUDAD_LABEL;
 
-  constructor(private accountService: AccountService, private loginModalService: LoginModalService, private router: Router) {
+  constructor(
+    private accountService: AccountService,
+    private loginModalService: LoginModalService,
+    private router: Router,
+    private profesionService: ProfesionService,
+    private regionService: RegionesService
+  ) {
     this.router.events.subscribe(event => {
       if (event instanceof NavigationEnd) {
         gtag('config', 'UA-181764554-1', {
@@ -35,6 +58,9 @@ export class HomeComponent implements OnInit, OnDestroy {
     vid.muted = true;
     vid.loop = true;
     vid?.play();
+    this.traerCiudad();
+    this.cargarProfesiones();
+    this.traerProfesiones();
   }
 
   isAuthenticated(): boolean {
@@ -51,8 +77,70 @@ export class HomeComponent implements OnInit, OnDestroy {
     }
   }
 
+  private _filterCiudades(value: string): string[] {
+    this.ciudades = [];
+    const filterValue = value.toLowerCase();
+    for (const valor of this.data) {
+      this.ciudades.push(valor.municipio + ' (' + valor.departamento + ')');
+    }
+    return this.ciudades.filter(option => option.toLowerCase().startsWith(filterValue)).sort();
+  }
+
+  private _filterProfesiones(value: string): IProfesion[] {
+    this.profesiones = [];
+    const filterValue = value.toLowerCase();
+    return this.profesiones.filter(option => option.profesion?.toLowerCase().includes(filterValue));
+    // for (const valor of this.dataProf) {
+    //   this.profesiones.push(valor.profesion);
+    // }
+    // return this.profesiones.filter(option => option.toLowerCase().startsWith(filterValue)).sort();
+  }
+
+  traerCiudad(): void {
+    this.regionService
+      .query({
+        page: 0,
+        size: 1150
+      })
+      .subscribe((res: HttpResponse<IRegiones[]>) => {
+        // this.region = res.body || [];
+        this.data = res.body;
+        this.filteredOptionsCiudades = this.myControlCiudades.valueChanges.pipe(
+          startWith(''),
+          map(value => this._filterCiudades(value))
+        );
+      });
+  }
+
+  traerProfesiones(): void {
+    this.filteredOptionsProfesiones = this.myControlProfesiones.valueChanges.pipe(
+      startWith(''),
+      map(value => this._filterProfesiones(value))
+    );
+  }
+
+  cargarProfesiones(): void {
+    this.profesionService
+      .query({
+        page: 0,
+        size: 550
+      })
+      .subscribe((res: HttpResponse<IProfesion[]>) => (this.profesiones = res.body || []));
+    // .subscribe((res: HttpResponse<IProfesion[]>) => {
+    //   this.dataProf = res.body;
+    //   this.filteredOptionsProfesiones = this.myControlProfesiones.valueChanges.pipe(
+    //     startWith(''),
+    //     map(value => this._filterProfesiones(value))
+    //   );
+    // });
+  }
+
   abrirAgregarUsuario(): void {
-    this.router.navigate(['/agregar-usuario']);
+    this.router.navigate(['/agregar-usuario', { userType: 'natural' }]);
+  }
+
+  registrarHojaVidaEmpresa(): void {
+    this.router.navigate(['/agregar-usuario', { userType: 'juridico' }]);
   }
 
   ventanaInicioSesion(): void {
